03e59740b0b9d3f11d7ea316a616790d0743f80f
Emmanuel326 <nyariboemmanuel8@gmail.com>
2025-03-17 15:36:43 +0300

    Update front end and back end code with latest changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,11 @@
-// next-sitemap.config.js
+
+
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.fynanceguide.site', 
-    generateRobotsTxt: true, 
-    
-  }
-  
+  siteUrl: 'https://fynanceguide.site',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  generateIndexSitemap: false,
+  outDir: 'public', // This ensures the sitemap is saved correctly
+  exclude: ["/admin", "/api/*"],
+};
