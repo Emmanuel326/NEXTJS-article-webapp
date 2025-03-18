4c82e1f7b65e3e9d4303859632cbcdf13b64608b
Emmanuel326 <nyariboemmanuel8@gmail.com>
2025-03-18 15:39:26 +0300

    optimized lazy loading and caching

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://fynanceguide.site/api/";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://api.fynanceguide.site/api/";
 
 const fetchData = async (endpoint, params = {}) => {
   try {
