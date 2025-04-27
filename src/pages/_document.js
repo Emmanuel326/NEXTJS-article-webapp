d8a9873604337dbd49848d8910c0a2f1bc42bc7a
Emmanuel326 <nyariboemmanuel8@gmail.com>
2025-04-27 22:12:05 +0300

    new minor update on _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,6 @@
 // src/pages/_document.js
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script'; // Import the next/script component
 
 class MyDocument extends Document {
   render() {
@@ -17,6 +18,24 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+
+          {/* Infolinks Ad Code - Load asynchronously using Next.js Script component */}
+          <Script
+            id="infolinks-inline-script" // Add a unique ID for the inline script
+            type="text/javascript"
+            strategy="afterInteractive" // Makes the script load after the page is interactive
+          >
+            {`
+              var infolinks_pid = 3435655;
+              var infolinks_wsid = 0;
+            `}
+          </Script>
+          <Script
+            id="infolinks-main-script" // Add a unique ID for the external script
+            type="text/javascript"
+            src="//resources.infolinks.com/js/infolinks_main.js"
+            strategy="afterInteractive" // Ensures the script loads asynchronously after the page is interactive
+          />
         </body>
       </Html>
     );
